dcb6ed0a4d082c37a0b70699b41d16c92197427d
akarduna <alexkarduna@gmail.com>
2025-02-04 12:30:31 -0800

    fixed the Mission to match new grid

--- og-motors/src/Components/Mission.js
+++ og-motors/src/Components/Mission.js
@@ -1,5 +1,5 @@
 import { Grid2, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, height } from "@mui/system";
 
 export default function Mission(){
     return (
@@ -10,40 +10,40 @@ export default function Mission(){
             }}
             height="100vh"
         >
-            <Grid2 container spacing={0} rowSpacing={20}>
-                <Grid2 size={10}>
-                    <Box
-                        sx={{
-                            width:"100%",
-                            backgroundColor:"#CCCFD0"
-                        }}
-                    >
-                        <Grid2 container spacing={0}>
-                            <Grid2 display="flex" offset={2} textAlign="center" size="auto" justifyContent="center" alignContent="center">
-                                <Typography color="#2D3436" variant="h2">
-                                    Our Mission
-                                </Typography>
-                            </Grid2>
-                        </Grid2>
-                    </Box>
-                </Grid2>
-                <Grid2 size={10} offset={2}>
-                    <Box
-                        sx={{
-                            backgroundColor:"#CCCFD0",
-                            paddingLeft:"8%",
-                            paddingRight:"20%",
-                            paddingTop:"4%",
-                            paddingBottom:"4%"
-                        }}
-                    >
-                        <Typography color="#2D3436" variant="body1">
-                            At OG Motors, we are committed to delivering a safe, luxurious, and unforgettable driving experience. Our mission is to redefine travel by offering state-of-the-art Tesla vehicles paired with exceptional hospitality. We prioritize your comfort and peace of mind, ensuring every journey blends innovation, elegance, and thoughtful care. Explore the future of driving with confidence and style-
-                            <br></br>your experience begins here
-                        </Typography>
-                    </Box>
+        <Box
+            sx={{
+                width:"90%",
+                backgroundColor:"#CCCFD0"
+            }}
+        >
+            <Grid2 container justifySelf="flex-end" spacing={12} columns={12} width="88.888888%" >
+                <Grid2 offset={1} size={12}>
+                    <Typography color="#2D3436" variant="h2">
+                        Our Mission
+                    </Typography>
                 </Grid2>
             </Grid2>
+        </Box>
+        <Box sx={{height:"30%"}}></Box>
+        <Box
+            sx={{
+                width:"90%",
+                justifySelf:"flex-end",
+                backgroundColor:"#CCCFD0",
+                paddingTop:"2%",
+                paddingBottom:"2%"
+            }}
+        >
+            <Grid2 container justifySelf="flex-start" spacing={12} columns={12} width="88.888888%" >
+                <Grid2 offset={1} size={12}>
+                    <Typography color="#2D3436" variant="body5">
+                        At OG Motors, we are committed to delivering a safe, luxurious, and unforgettable driving experience. Our mission is to redefine travel by offering state-of-the-art Tesla vehicles paired with exceptional hospitality. We prioritize your comfort and peace of mind, ensuring every journey blends innovation, elegance, and thoughtful care. Explore the future of driving with confidence and style-
+                        <br></br>your experience begins here
+                    </Typography>
+                </Grid2>
+            </Grid2>
+           
+            </Box>
         </Box>
     );
 }
